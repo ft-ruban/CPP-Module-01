0f9b2bd4360bb359394e1e3e7e3fc8d7fe30a6e5
Ruban <Ruban@example.com>
2025-12-15 13:28:39 +0100

    redone ex02

---
+++ ex02/HI_THIS_IS_BRAIN.mk
@@ -0,0 +1,8 @@
+override SRCSDIR	:= srcs/
+override MAINDIR    :=
+
+SRC += $(addprefix $(MAINDIR), $(addsuffix .cpp, $(MAIN)))
+
+override SRCS		= $(addprefix $(SRCSDIR), $(SRC))
+
+override MAIN := main 
